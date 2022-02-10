b68461e3c75145ec30d9ae313d50ec9a2f70380b
rachalsmith <66318063+rachalSmith@users.noreply.github.com>
2022-02-10 10:24:35 +0000

    Delete src/components directory

--- src/components/AverageCalculation.js
+++
@@ -1,9 +0,0 @@
-function AverageCalculation() {
-    return (
-      <div className="average-calculation">
-        <p>Average Calculation</p>
-      </div>
-    );
-  }
-
-  export default AverageCalculation;
